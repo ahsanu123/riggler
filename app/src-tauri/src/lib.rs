f9eb9e0936d853c40acc57fe56f0eea9f8f5673d
Ahsanu Amala <caasperahsanuamala5@gmail.com>
2026-01-11 06:48:38 +0700

    feat: create tauri command for save, get, and toggle jiggling

--- app/src-tauri/src/lib.rs
+++ app/src-tauri/src/lib.rs
@@ -1,14 +1,23 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use tauri::AppHandle;
+
+use crate::store_command::{get_config, set_config, toggle_jiggling};
+
+mod store_command;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    riggler_shared::jiggling();
+
     tauri::Builder::default()
+        .plugin(tauri_plugin_window_state::Builder::new().build())
+        .plugin(tauri_plugin_store::Builder::new().build())
+        .plugin(tauri_plugin_single_instance::init(|_app, _args, _cwd| {}))
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![
+            get_config,
+            set_config,
+            toggle_jiggling
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
---
+++ app/src-tauri/src/store_command.rs
@@ -0,0 +1,89 @@
+use riggler_shared::JIGGLING_ENABLE;
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
+use serde_json::Value;
+use std::sync::atomic::Ordering;
+use tauri::{ipc::InvokeError, AppHandle};
+use tauri_plugin_store::StoreExt;
+
+const RIGGLER_SETTING: &str = "riggler.setting.json";
+const RIGGLER_SETTING_KEY: &str = "riggler_setting";
+
+#[derive(Debug)]
+pub enum RigglerErr {
+    GetStoreProviderErr,
+    SetConfigErr,
+    GetConfigErr,
+    ToggleJigglingErr,
+    SaveStoreErr,
+}
+
+impl Into<InvokeError> for RigglerErr {
+    fn into(self) -> InvokeError {
+        match self {
+            RigglerErr::GetStoreProviderErr => InvokeError::from("error when get store provider"),
+            RigglerErr::SetConfigErr => InvokeError::from("error when setting config"),
+            RigglerErr::GetConfigErr => InvokeError::from("error when get config"),
+            RigglerErr::ToggleJigglingErr => InvokeError::from("error when toggle jiggling"),
+            RigglerErr::SaveStoreErr => InvokeError::from("error save config"),
+        }
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct RigglerConfig {
+    jiggling_duration: u32,
+}
+
+impl RigglerConfig {
+    pub fn new(jiggling_duration: u32) -> Self {
+        Self { jiggling_duration }
+    }
+}
+
+impl Default for RigglerConfig {
+    fn default() -> Self {
+        Self {
+            jiggling_duration: 1,
+        }
+    }
+}
+
+#[tauri::command]
+pub fn toggle_jiggling() -> bool {
+    let jiggling = JIGGLING_ENABLE.load(Ordering::Relaxed);
+
+    JIGGLING_ENABLE.store(!jiggling, Ordering::Relaxed);
+
+    jiggling
+}
+
+#[tauri::command]
+pub fn get_config(app: AppHandle) -> Result<RigglerConfig, RigglerErr> {
+    let store = app
+        .store(RIGGLER_SETTING)
+        .map_err(|_| RigglerErr::GetStoreProviderErr)?;
+
+    let riggler_config_value = store.get(RIGGLER_SETTING_KEY);
+
+    if riggler_config_value.is_none() {
+        return Ok(RigglerConfig::default());
+    } else {
+        let config = serde_json::from_value::<RigglerConfig>(riggler_config_value.unwrap())
+            .map_err(|_| RigglerErr::GetConfigErr)?;
+        return Ok(config);
+    }
+}
+
+#[tauri::command]
+pub fn set_config(app: AppHandle, config: RigglerConfig) -> Result<bool, RigglerErr> {
+    let store = app
+        .store(RIGGLER_SETTING)
+        .map_err(|_| RigglerErr::GetStoreProviderErr)?;
+
+    let config_value = serde_json::to_value(config).map_err(|_| RigglerErr::SetConfigErr)?;
+
+    store.set(RIGGLER_SETTING_KEY, config_value);
+    store.save().map_err(|_| RigglerErr::SaveStoreErr)?;
+
+    Ok(true)
+}
