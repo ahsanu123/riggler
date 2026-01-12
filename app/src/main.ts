d970ddda56ff7cdc63bddf2e178c04419b5afed7
Ahsanu Amala <81602442+ahsanu123@users.noreply.github.com>
2026-01-12 18:52:47 +0700

    feat: use mithril js

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,79 +1,187 @@
-import { getConfig, setConfig, toggleJiggling } from "./invokes"
+import m, { render } from 'mithril'
+import './styles.css'
 
-let trayButton: HTMLButtonElement | null
-let settingButton: HTMLButtonElement | null
+const mainContainer = document.getElementById("main-container")
 
-let jigglingCheckbox: HTMLInputElement | null
-let jigglingDuration: HTMLInputElement | null
-let jigglingDurationLabel: HTMLLabelElement | null
-
-let settingContainer: HTMLDivElement | null
-let helpContainer: HTMLDivElement | null
-
-let state = {
-  isSettingHidden: true,
-  isJiggling: false,
-  jigglingDuration: 1
+enum ActiveTab {
+  MainScreen,
+  Setting,
+  About
 }
 
-window.addEventListener("DOMContentLoaded", () => {
+interface RigglerAppState {
+  activeTab: ActiveTab,
+  jigglingDuration: number,
+  jigglingDelta: number,
+}
 
-  trayButton = document.getElementById("minimize-to-tray-button") as HTMLButtonElement
-  settingButton = document.getElementById("setting-button") as HTMLButtonElement
+const rigglerAppState: RigglerAppState = {
+  activeTab: ActiveTab.MainScreen,
+  jigglingDuration: 1,
+  jigglingDelta: 1
+}
 
-  jigglingCheckbox = document.getElementById("jiggling-checkbox") as HTMLInputElement
-  jigglingDuration = document.getElementById("jiggling-duration") as HTMLInputElement
-  jigglingDurationLabel = document.getElementById("jiggling-duration-label") as HTMLLabelElement
+const mainScreenTab: m.Component = {
+  view: function (): m.Children | null | void {
+    return m(".main-screen",
+      [
+        m('img', {
+          onclick: () => rigglerAppState.activeTab = ActiveTab.MainScreen,
+          class: 'riggler-icon-image',
+          src: '/src/assets/riggler-icon.png',
+          height: '70px',
+          style: {
+            borderRadius: "60px"
+          }
+        }),
 
-  settingContainer = document.getElementById("riggler-setting") as HTMLDivElement
-  helpContainer = document.getElementById("riggler-about") as HTMLDivElement
+        m('.jiggling-container',
+          [
+            m("h2", "Riggler"),
+            m("div", [
+              m("input[type='checkbox']", {
+                id: "is-jiggling-checkbox",
+                name: "is-jiggling-checkbox"
+              }),
+              m("label",
+                { for: "is-jiggling-checkbox" },
+                "Jiggling"
+              )
+            ])
+          ]
+        ),
 
-  getConfig().then(value => {
-    const duration = value?.jiggling_duration ?? 1
-    state.jigglingDuration = duration
-    jigglingDurationLabel!.innerText = `Jiggling Duration: ${duration}`
-    jigglingDuration!.value = duration.toString()
-  })
+        m(".button-container",
+          [
+            m('button', {
+              onclick: () => console.log("todo minimize to tray")
+            },
+              m("img", {
+                width: "50px",
+                src: "/src/assets/chevron-down-icon.svg"
+              })
+            ),
 
-  const handleOnTrayButtonClick = () => {
-    // TODO: invoke tauri or something
-    // need reading more about system tray
+            m('button', {
+              onclick: () => rigglerAppState.activeTab = ActiveTab.Setting
+            },
+              m("img", {
+                width: "50px",
+                src: "/src/assets/gear-icon.svg"
+              })
+            ),
+
+            m('button', {
+              onclick: () => rigglerAppState.activeTab = ActiveTab.About
+            },
+              m("img", {
+                width: "50px",
+                src: "/src/assets/question-mark.svg"
+              })
+            ),
+          ]
+        )
+      ]
+    )
   }
+}
 
-  const toggleSettingContainerVisibility = () => {
-    if (state.isSettingHidden) {
-      settingContainer?.removeAttribute('hidden')
-      state.isSettingHidden = false
+const aboutScreenTab: m.Component = {
+  view: function (): m.Children | null | void {
+    return m(".about-screen", [
+      m('img', {
+        onclick: () => rigglerAppState.activeTab = ActiveTab.MainScreen,
+        class: 'riggler-icon-image',
+        src: '/src/assets/riggler-icon.png',
+        height: '70px',
+        style: {
+          borderRadius: "60px"
+        }
+      }),
 
-      helpContainer?.setAttribute('hidden', '')
+      m("div", [
+        m("h2", "About Riggler"),
+        m("p", {
+          style: { padding: '0 10px 0 0' }
+        },
+          `
+            Die Studierenden sollen die Zusammenhänge zwischen den verschiedenen Größen der
+            Feldtheorie verstehen und in mathematischer
+            Form anwenden können. Darüber hinaus
+            sollen sie die Vorgänge in stationären, qu
+          `)
+      ])
+    ])
+  }
+}
+
+const settingScreenTab: m.Component = {
+  view: function (): m.Children | null | void {
+    return m(".setting-screen", [
+      m('img', {
+        onclick: () => rigglerAppState.activeTab = ActiveTab.MainScreen,
+        class: 'riggler-icon-image',
+        src: '/src/assets/riggler-icon.png',
+        height: '70px',
+        style: {
+          borderRadius: "60px"
+        }
+      }),
+
+      m("div",
+        [
+          m("label", { for: "jiggling-duration" }, `Jiggling Duration ${rigglerAppState.jigglingDuration}`),
+          m("br"),
+          m("input[type='range']", {
+            id: "jiggling-duration",
+            name: "jiggling-duration",
+            class: "jiggling-duration",
+            min: 1,
+            max: 10,
+            value: rigglerAppState.jigglingDuration
+          }),
+
+          m("br"),
+          m("label", { for: "jiggling-delta" }, `Jiggling Delta ${rigglerAppState.jigglingDelta}`),
+          m("br"),
+          m("input[type='range']", {
+            id: "jiggling-delta",
+            name: "jiggling-delta",
+            class: "jiggling-delta",
+            min: 1,
+            max: 10,
+            value: rigglerAppState.jigglingDuration
+          }),
+        ]
+      )
+    ])
+  }
+}
+
+const rigglerApp: m.Component = {
+
+  view: function (): m.Children | null | void {
+
+    const renderActiveTab = () => {
+
+      switch (rigglerAppState.activeTab) {
+        case ActiveTab.MainScreen:
+          return m(mainScreenTab)
+
+        case ActiveTab.Setting:
+          return m(settingScreenTab)
+
+        case ActiveTab.About:
+          return m(aboutScreenTab)
+
+        default:
+          return m(mainScreenTab)
+      }
     }
-    else {
-      settingContainer?.setAttribute('hidden', '')
-      state.isSettingHidden = true
 
-      helpContainer?.removeAttribute('hidden')
-    }
+    return m('.riggler-app', renderActiveTab())
   }
+}
 
-  const handleOnJigglingCheckedChange = async (_ev: Event) => {
-    const result = await toggleJiggling()
-    console.log("toggleJiggling", result)
-  }
+m.mount(mainContainer!, rigglerApp)
 
-  const handleOnJigglingDurationChange = async (ev: Event) => {
-    const range = ev.target as HTMLInputElement
-    jigglingDurationLabel!.innerText = `Jiggling Duration: ${range.value}`
-
-    state.jigglingDuration = Number(range.value)
-    const result = await setConfig({ jiggling_duration: state.jigglingDuration })
-
-    console.log("setconfig", result)
-  }
-
-  trayButton.addEventListener('click', handleOnTrayButtonClick)
-  settingButton.addEventListener('click', toggleSettingContainerVisibility)
-
-  jigglingCheckbox.addEventListener('change', handleOnJigglingCheckedChange)
-  jigglingDuration.addEventListener('change', handleOnJigglingDurationChange)
-
-});
