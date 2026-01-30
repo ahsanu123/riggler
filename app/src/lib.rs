3bce88e33eec90c12e379fae4a9c13f29cd521dd
Ahsanu Amala <caasperahsanuamala5@gmail.com>
2026-01-30 22:33:17 +0700

    feat: slint basic user interface is done

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -7,9 +7,34 @@ fn ui() -> MainWindow {
     MainWindow::new().unwrap()
 }
 
+fn screen_as_string(screen: Screen) -> String {
+    match screen {
+        Screen::Jiggling => String::from("Jiggling"),
+        Screen::Setting => String::from("Setting"),
+        Screen::About => String::from("About"),
+    }
+}
+
+fn config_as_string(config: Configuration) -> String {
+    format!("delay: {}, delta: {}", config.delay, config.delta)
+}
+
 #[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
 pub fn main() {
     let ui = ui();
+    let global_state = ui.global::<RigglerState>();
+
+    global_state.on_onActiveScreenChanged(|screen| {
+        println!("screen -> {}", screen_as_string(screen));
+    });
+
+    global_state.on_onIsJigglingChanged(|is_jiggling| {
+        println!("is_jiggling -> {}", is_jiggling);
+    });
+    global_state.on_onConfigurationChanged(|config| {
+        println!("config -> {}", config_as_string(config));
+    });
+
     ui.run().unwrap();
 }
 
--- app/src/main.rs
+++ app/src/main.rs
@@ -2,5 +2,5 @@
 // Just forward to the library in main
 
 fn main() {
-    my_app_lib::main();
+    riggler_lib::main();
 }
